37253759d752994018c2fe59610375aba74dd0bf
Hardik Sharma <kumarankit2002dseu@gmail.com>
2026-01-21 15:17:58 +0530

    added deployed backend link

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL :"https://checkinn-1-hu98.onrender.com",
+    baseURL : import.meta.env.VITE_API_BASE_URL,
      withCredentials: true,
 })
 
